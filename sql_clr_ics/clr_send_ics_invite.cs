58a6a1b438a74b61875c2d12068a3b05d568412c
Eitan Blumin <flatline972@hotmail.com>
2019-05-23 12:20:10 +0300

    refactored parameters, and started implementation for using sysmail_profile

--- sql_clr_ics/clr_send_ics_invite.cs
+++ sql_clr_ics/clr_send_ics_invite.cs
@@ -3,26 +3,70 @@ using System.Text;
 using System.Net;
 using System.Net.Mail;
 using System.Data.SqlTypes;
+using System.Data.SqlClient;
+using Microsoft.SqlServer.Server;
 
 public partial class StoredProcedures
 {
     [Microsoft.SqlServer.Server.SqlProcedure]
     public static void clr_send_ics_invite(
-          SqlString from, SqlString to, SqlString cc, SqlString reply_to
-        , SqlString subject, SqlString body, SqlString location
-        , SqlDateTime start_time_utc, SqlDateTime end_time_utc, SqlDateTime timestamp_utc
-        , SqlString smtp_server, SqlInt32 port, SqlBoolean use_ssl, SqlString username, SqlString password
+          SqlString profile_name
+        , SqlString recipients, SqlString copy_recipients, SqlString blind_copy_recipients
+        , SqlString from_address, SqlString reply_to
+        , SqlString subject, SqlString body
+        , SqlString importance, SqlString sensitivity, SqlString file_attachments
+        , SqlString location, SqlDateTime start_time_utc, SqlDateTime end_time_utc, SqlDateTime timestamp_utc
+        , SqlString method, SqlInt32 sequence, SqlString prod_id
         , SqlBoolean use_reminder, SqlInt32 reminder_minutes, SqlBoolean require_rsvp
-        , SqlGuid cancel_event_identifier, out SqlGuid event_identifier
+        , SqlString smtp_servername, SqlInt32 port, SqlBoolean enable_ssl
+        , SqlString username, SqlString password
         , SqlBoolean suppress_info_messages
+        , ref SqlGuid event_identifier
         )
     {
+        #region get missing info from sysmail profile
+
+        if (from_address.IsNull)
+        {
+            SqlConnection con = new SqlConnection("context connection=true"); // using existing CLR context connection
+            SqlCommand cmd = con.CreateCommand();
+            con.Open();
+
+            if (profile_name.IsNull)
+            {
+                cmd.CommandText = @"SELECT p.name
+FROM [msdb].[dbo].[sysmail_principalprofile] AS pp
+INNER JOIN [msdb].[dbo].[sysmail_profile] AS p
+ON pp.profile_id = p.profile_id
+WHERE pp.is_default = 1";
+
+                using (SqlDataReader rdr = cmd.ExecuteReader())
+                {
+                    if (!rdr.HasRows)
+                    {
+                        rdr.Close();
+                        con.Close();
+                        throw new Exception("profile_name not specified and no default profile found");
+                    } else
+                    {
+                        profile_name = rdr.GetSqlString(0);
+                    }
+                    rdr.Close();
+                }
+            }
+
+
+            con.Close();
+        }
+
+        #endregion get missing info from sysmail profile
+
         #region validations
 
         StringBuilder sb_Errors = new StringBuilder();
 
-        if (from.IsNull || String.IsNullOrEmpty(from.Value)) sb_Errors.AppendLine("Missing parameter: from");
-        if (to.IsNull || String.IsNullOrEmpty(to.Value)) sb_Errors.AppendLine("Missing parameter: to");
+        if (from_address.IsNull || String.IsNullOrEmpty(from_address.Value)) sb_Errors.AppendLine("Missing parameter: from");
+        if (recipients.IsNull || String.IsNullOrEmpty(recipients.Value)) sb_Errors.AppendLine("Missing parameter: to");
         if (subject.IsNull || String.IsNullOrEmpty(subject.Value)) sb_Errors.AppendLine("Missing parameter: subject");
 
         if (sb_Errors.Length > 0) throw new Exception("Unable to send mail due to validation error(s): " + sb_Errors);
@@ -36,10 +80,11 @@ public partial class StoredProcedures
         if (reminder_minutes.IsNull) reminder_minutes = 15;
         if (use_reminder.IsNull) use_reminder = true;
         if (suppress_info_messages.IsNull) suppress_info_messages = false;
+        if (prod_id.IsNull || String.IsNullOrEmpty(prod_id.Value)) prod_id = "-//Schedule a Meeting";
 
-        if (smtp_server.IsNull || String.IsNullOrEmpty(smtp_server.Value)) smtp_server = "localhost";
+        if (smtp_servername.IsNull || String.IsNullOrEmpty(smtp_servername.Value)) smtp_servername = "localhost";
         if (port.IsNull) port = 25;
-        if (use_ssl.IsNull) use_ssl = false;
+        if (enable_ssl.IsNull) enable_ssl = false;
         if (timestamp_utc.IsNull) timestamp_utc = DateTime.UtcNow;
 
         bool useDefaultCredentials = false;
@@ -55,14 +100,9 @@ public partial class StoredProcedures
             credentials = new NetworkCredential(username.Value, password.Value);
         }
 
-        if (!cancel_event_identifier.IsNull)
-            event_identifier = cancel_event_identifier.Value;
-        else
-            event_identifier = Guid.NewGuid();
-
         #endregion default values initialization
 
-        #region initialize MailMessage and recipients
+#region initialize MailMessage and recipients
 
         MailMessage msg = new MailMessage();
         msg.Subject = subject.Value;
@@ -70,7 +110,7 @@ public partial class StoredProcedures
 
         try
         {
-            msg.From = new MailAddress(from.Value);
+            msg.From = new MailAddress(from_address.Value);
         }
         catch (Exception e)
         {
@@ -88,7 +128,7 @@ public partial class StoredProcedures
 
         try
         {
-            msg.To.Add(to.Value.Replace(';', ','));
+            msg.To.Add(recipients.Value.Replace(';', ','));
         }
         catch (Exception e)
         {
@@ -97,28 +137,28 @@ public partial class StoredProcedures
 
         try
         {
-            if (!cc.IsNull && !String.IsNullOrEmpty(cc.Value)) msg.CC.Add(cc.Value.Replace(';', ','));
+            if (!copy_recipients.IsNull && !String.IsNullOrEmpty(copy_recipients.Value)) msg.CC.Add(copy_recipients.Value.Replace(';', ','));
         }
         catch (Exception e)
         {
             throw new Exception("CC address is invalid: " + e.Message);
         }
 
-        #endregion initialize MailMessage and recipients
+#endregion initialize MailMessage and recipients
 
-        #region construct ICS file contents
+#region construct ICS file contents
 
         StringBuilder str = new StringBuilder();
         str.AppendLine("BEGIN:VCALENDAR");
-        str.AppendLine("PRODID:-//Schedule a Meeting");
+        str.AppendLine(string.Format("PRODID:{0}", prod_id.Value));
         str.AppendLine("VERSION:2.0");
 
-        if (cancel_event_identifier.IsNull)
+        if (method == "request")
         {
             str.AppendLine("METHOD:REQUEST");
             str.AppendLine("SEQUENCE:0");
         }
-        else
+        else if (method == "cancel")
         {
             str.AppendLine("METHOD:CANCEL");
             str.AppendLine("SEQUENCE:1");
@@ -146,7 +186,7 @@ public partial class StoredProcedures
 
         if (!organizer_in_recipients) str.AppendLine(string.Format("ATTENDEE;CUTYPE=INDIVIDUAL;ROLE=NON-PARTICIPANT;PARTSTAT=ACCEPTED;RSVP=FALSE;CN=\"{0}\";X-NUM-GUESTS=0:mailto:{1}", msg.From.DisplayName, msg.From.Address));
 
-        if (use_reminder && cancel_event_identifier.IsNull)
+        if (use_reminder && method.IsNull)
         {
             str.AppendLine("BEGIN:VALARM");
             str.AppendLine(string.Format("TRIGGER:-PT{0}M", reminder_minutes.Value));
@@ -158,18 +198,18 @@ public partial class StoredProcedures
         str.AppendLine("END:VEVENT");
         str.AppendLine("END:VCALENDAR");
 
-        #endregion construct ICS file contents
+#endregion construct ICS file contents
 
-        #region initialize and configure SmtpClient
+#region initialize and configure SmtpClient
 
         System.Net.Mail.SmtpClient smtpclient = new System.Net.Mail.SmtpClient();
 
         try
         {
-            smtpclient.Host = smtp_server.Value;
+            smtpclient.Host = smtp_servername.Value;
             smtpclient.Port = port.Value;
             smtpclient.UseDefaultCredentials = useDefaultCredentials;
-            smtpclient.EnableSsl = use_ssl.Value;
+            smtpclient.EnableSsl = enable_ssl.Value;
             smtpclient.Credentials = credentials;
             System.Net.Mime.ContentType contype = new System.Net.Mime.ContentType("text/calendar");
             contype.Parameters.Add("method", "REQUEST");
@@ -187,13 +227,13 @@ public partial class StoredProcedures
             throw new Exception("SMTP Client Configuration Error: " + e.Message);
         }
 
-        #endregion initialize and configure SmtpClient
+#endregion initialize and configure SmtpClient
 
         try
         {
             smtpclient.Send(msg);
             if (!suppress_info_messages)
-                Microsoft.SqlServer.Server.SqlContext.Pipe.Send(string.Format("Mail Sent. Event Identifier: {0}", event_identifier.Value));
+                SqlContext.Pipe.Send(string.Format("Mail Sent. Event Identifier: {0}", event_identifier.Value));
         }
         catch (Exception e)
         {
