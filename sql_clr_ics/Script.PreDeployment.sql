18f723da59a1e06d1cfaa19455947121524ea1d0
Eitan Blumin <eitan@madeiradata.com>
2020-04-16 09:56:32 +0300

    Deployment fixes

--- sql_clr_ics/Script.PreDeployment.sql
+++ sql_clr_ics/Script.PreDeployment.sql
@@ -9,16 +9,50 @@
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
+-- Make sure clr is enabled
+DECLARE @InitAdvanced INT
+
+IF EXISTS (select * from sys.configurations where name IN ('clr enabled') and value_in_use = 0)
+BEGIN
+	SELECT @InitAdvanced = CONVERT(int, value) FROM sys.configurations WHERE name = 'show advanced options';
+
+	IF @InitAdvanced = 0
+	BEGIN
+		EXEC sp_configure 'show advanced options', 1;
+		RECONFIGURE;
+	END
+
+	IF EXISTS (select * from sys.configurations where name = 'clr enabled' and value_in_use = 0)
+	BEGIN
+		EXEC sp_configure 'clr enabled', 1;
+		RECONFIGURE;
+	END
+
+	IF @InitAdvanced = 0
+	BEGIN
+		EXEC sp_configure 'show advanced options', 0;
+		RECONFIGURE;
+	END
+END
+GO
 use [master];
 GO
-IF NOT EXISTS (select * from sys.asymmetric_keys WHERE name = 'sql_clr_ics_pkey')
+-- Create symmetric key from DLL
+IF NOT EXISTS (
+		select * from master.sys.asymmetric_keys 
+		WHERE 
+			name = 'sql_clr_ics_pkey'
+			--thumbprint = 0xC5022B1D1415FC7A
+		)
 	create asymmetric key sql_clr_ics_pkey
 	from executable file = '$(PathToSignedDLL)'
 	--encryption by password = 'vtwjmifewVfnhrYke@ZuhxkumsFT7_&#$!~<avjqn|mnvJhp'
 GO
-IF NOT EXISTS (select name from sys.syslogins where name = 'sql_clr_ics_login')
+-- Create server login from symmetric key
+IF NOT EXISTS (select name from master.sys.syslogins where name = 'sql_clr_ics_login')
 	create login sql_clr_ics_login from asymmetric key sql_clr_ics_pkey;
 GO
+-- Grant UNSAFE ASSEMBLY permissions to login which was created from DLL signing key
 grant unsafe assembly to sql_clr_ics_login;
 GO
 -- Return execution context to intended target database
